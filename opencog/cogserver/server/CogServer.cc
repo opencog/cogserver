86a49aa5289ba22dfa5a507f417dc019d90444c9
Linas Vepstas <linasvepstas@gmail.com>
2019-11-15 16:21:46 -0600

    Fix failing unit test

--- opencog/cogserver/server/CogServer.cc
+++ opencog/cogserver/server/CogServer.cc
@@ -411,10 +411,12 @@ void CogServer::loadModules(std::vector<std::string> module_paths)
 {
     if (module_paths.empty())
     {
-        // XXX FIXME This hack allowsan installed cogserver to inadvertantly load from
-        // a fixed build path. This should be handled by the config file or test environment...
+        // XXX FIXME This hack allows an installed cogserver to
+        // inadvertantly load from a fixed build path. This should
+        // be handled by the config file or test environment...
         // not hard coded.
         // Give priority search order to the build directories
+        module_paths.push_back(PROJECT_BINARY_DIR "/opencog/cogserver/modules/");
         module_paths.push_back(PROJECT_BINARY_DIR "/opencog/cogserver/server/");
         module_paths.push_back(PROJECT_BINARY_DIR "/opencog/cogserver/shell/");
 
