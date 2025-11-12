df0ab7169a09fa8ee8fe6e8512577ad3c860e4c5
Linas Vepstas <linasvepstas@gmail.com>
2025-11-12 23:25:18 +0000

    MCP: Bump version number to 0.2.1

--- opencog/cogserver/mcp-eval/McpEval.cc
+++ opencog/cogserver/mcp-eval/McpEval.cc
@@ -117,7 +117,7 @@ void McpEval::eval_expr(const std::string &expr)
 			response["result"]["capabilities"]["resources"] = resourcesCapability;
 
 			response["result"]["serverInfo"]["name"] = "CogServer MCP";
-			response["result"]["serverInfo"]["version"] = "0.2.0";
+			response["result"]["serverInfo"]["version"] = "0.2.1";
 			response["result"]["serverInfo"]["instructions"] =
 				"The CogServer MCP provides access to a live, running instance of the "
 				"AtomSpace. It allows the MCP client to view and manipulate the contents "
